d9880c8306ceba6e153bfc64e70951b891c854df
Mohd. Ahmad <97238445+ahmad9652@users.noreply.github.com>
2023-01-03 01:27:00 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -14,7 +14,7 @@ export default function About(props) {
           <div className="accordion-item" >
             <h2 className="accordion-header" id="headingOne">
               <button className="accordion-button" style={{ color: props.mode === 'dark' ? 'white' : 'black', backgroundColor: props.mode === 'dark' ? 'grey' : 'white' }} type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                About TextUtils
+                About SMANotebook
               </button>
             </h2>
             <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
